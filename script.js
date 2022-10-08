a8acab9956f69514780b96a26502372904d7d8e7
Eren Cataltepe <erencataltepe@gmail.com>
2022-10-08 11:20:49 +0300

    Change score when button is clicked.

---
+++ script.js
@@ -0,0 +1,24 @@
+const homeScoreboard = document.getElementById("home-scoreboard");
+const guestScoreboard = document.getElementById("guest-scoreboard");
+const buttons = document.querySelectorAll("button");
+
+let homeScore = 0;
+let guestScore = 0;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const scoreOwner = e.target.parentElement.dataset.key;
+    const score = Number(e.target.textContent);
+    changeScore(scoreOwner, score);
+  });
+});
+
+function changeScore(scoreOwner, score) {
+  if (scoreOwner == "home") {
+    homeScore += score;
+    homeScoreboard.textContent = homeScore;
+  } else {
+    guestScore += score;
+    guestScoreboard.textContent = guestScore;
+  }
+}
